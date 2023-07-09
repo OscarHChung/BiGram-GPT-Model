0a66ea80eb35a110be8d6ee6849f60bce409776f
Oscar Chung <oscar.chung.pa@gmail.com>
2023-07-08 17:10:37 -0700

    test

--- bigram.py
+++ bigram.py
@@ -2,10 +2,11 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-batch_size = 32
-chunk_size = 8
-max_iters = 3000
+batch_size = 32 # number of parallel independent sequences to run
+chunk_size = 8 # max size of the chunks to run algo on
+max_iters = 3000 # max times to run algo
 eval_interval = 300
 learning_rate = 1e-2
 device ='cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
+
